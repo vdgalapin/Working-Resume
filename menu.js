61e0162e0b1b57220a10bf513191e4eb5b4d7f1f
Vyron Galapin <59787986+vdgalapin@users.noreply.github.com>
2020-02-29 13:58:57 -0600

    prototype 1 sidenav with animations

--- menu.js
+++ menu.js
@@ -1,12 +1,13 @@
 function change() {
 	document.getElementById("menu").classList.toggle("change");
-	document.getElementById("options").classList.toggle("show");
+	// document.getElementById("options").classList.toggle("show");
+	document.getElementById("options").style.width = 400;
 }
 
 window.onclick = function(event) {
 	if (!event.target.matches('.menu') && !event.target.matches('.bar1') && !event.target.matches('.bar2') && !event.target.matches('.bar3')) {
 		document.getElementById("menu").classList.remove("change");
-		document.getElementById("options").classList.remove("show");
-	  
+		// document.getElementById("options").classList.remove("show");
+		document.getElementById("options").style.width = 0;
 	}
 }
